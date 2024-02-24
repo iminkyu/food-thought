1c1cc637cb9cccde19aec29870322298fc40781e
dragonsky2357 <qkrdydals327@gmail.com>
2024-02-24 19:11:19 +0900

    add: 프로젝트 생성

---
+++ src/main/java/com/example/foodthought/FoodThoughtApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodthought;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodThoughtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodThoughtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/foodthought/FoodThoughtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodthought;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodThoughtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
