bc00547ea06326d2b53f59e0b7f83660acfadd93
stidy <gstudy1021@gmail.com>
2024-02-26 00:05:07 +0900

    수정 중

--- src/main/java/com/example/foodthought/repository/BoardRepository.java
+++ src/main/java/com/example/foodthought/repository/BoardRepository.java
@@ -1,25 +1,16 @@
 package com.example.foodthought.repository;
 
-
 import com.example.foodthought.entity.Board;
-<<<<<<< HEAD
+
 import com.example.foodthought.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
     List<Board> findAllByUser(User user);
-=======
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface BoardRepository extends JpaRepository<Board, Long> {
-
->>>>>>> f2cb2fa (board dto, entity 구현 완료)
 }
 
