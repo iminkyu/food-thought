7cb4226d84243bc76bdaeb135fb92f929b46c291
dragonsky2357 <qkrdydals327@gmail.com>
2024-02-25 22:50:25 +0900

    add: 로그인 기능 추가

---
+++ src/main/java/com/example/foodthought/security/UserDetailsServiceImpl.java
@@ -0,0 +1,22 @@
+package com.example.foodthought.security;
+
+import com.example.foodthought.entity.User;
+import com.example.foodthought.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+        User user = userRepository.findByUserId(userId)
+                .orElseThrow(() -> new UsernameNotFoundException("Not Found User"));
+
+        return new UserDetailsImpl(user);
+    }
+}
