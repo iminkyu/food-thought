1be34c402d3e3ad991e56ce10759c5b831017477
pshistory <psh_a@naver.com>
2024-02-27 12:46:30 +0900

    update: follow 가장많은 3명 조회기능 구현 add: 본인을 follow 할 수 없게 로직 추가

--- src/main/java/com/example/foodthought/controller/FollowController.java
+++ src/main/java/com/example/foodthought/controller/FollowController.java
@@ -7,22 +7,25 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/users/{userId}/follows")
+@RequestMapping("/api/users")
 public class FollowController {
     private final FollowService followService;
 
 
     //팔로잉/취소
-    @PostMapping
+    @PostMapping("/{userId}/follows")
     public ResponseEntity<ResponseDto> toggleFollow(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long userId) {
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body(followService.toggleFollow(userDetails.getUser(), userId));
+    }
+
+    //가장 follow 많은 회원 3명
+    @GetMapping("/follows-top3")
+    public ResponseEntity<ResponseDto> findFollowerByLikeTop3() {
+        return ResponseEntity.status(201).body(followService.findFollowerByLikeTop3());
     }
 }
 
