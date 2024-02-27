3605a9a104584614a9609202a6c7c917a61138ce
pshistory <psh_a@naver.com>
2024-02-27 20:52:55 +0900

    fix: Book Entity 생성으로 Board Entity에 있던 책 제목등 책관련 내용 이전하면서 Board에 관련된 Dto, Like 등 필드값 변경

--- src/main/java/com/example/foodthought/dto/board/UpdateBoardRequestDto.java
+++ src/main/java/com/example/foodthought/dto/board/UpdateBoardRequestDto.java
@@ -3,26 +3,14 @@ package com.example.foodthought.dto.board;
 
 import lombok.*;
 
+import java.awt.print.Book;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class UpdateBoardRequestDto {
-    private String booktitle;
-    private String bookauthor;
-    private String bookpublisher;
-    private String bookimage;
-    private String bookcategory;
+    private Book book;
     private String contents;
-
-    UpdateBoardRequestDto updateBoardRequestDto = UpdateBoardRequestDto.builder()
-            .booktitle(booktitle)
-            .bookauthor(bookauthor)
-            .bookpublisher(bookpublisher)
-            .bookimage(bookimage)
-            .bookcategory(bookcategory)
-            .contents(contents)
-            .build();
-
 }
--- src/main/java/com/example/foodthought/entity/Board.java
+++ src/main/java/com/example/foodthought/entity/Board.java
@@ -25,25 +25,11 @@ public class Board {
     @JoinColumn(name = "users_id")
     private User users;
 
-    //book entity와의 연관관계 생성
+
     @ManyToOne
     @JoinColumn(name = "books_id")
     private Book book;
 
-    @Column(nullable = false, length = 255)
-    private String booktitle;
-
-    @Column(nullable = false, length = 255)
-    private String bookauthor;
-
-    @Column(nullable = false, length = 255)
-    private String bookpublisher;
-
-    @Column(nullable = false, length = 65535)
-    private String bookimage;
-
-    @Column(nullable = false, length = 255)
-    private String bookcategory;
 
     @Column(nullable = false, length = 65535)
     private String contents;
