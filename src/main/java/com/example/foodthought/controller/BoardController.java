1d706341c3d325f3d9c45f1ca84d46022f6120c0
stidy <gstudy1021@gmail.com>
2024-02-28 14:35:56 +0900

    :memo: Board 관련 작업 현황

--- src/main/java/com/example/foodthought/controller/BoardController.java
+++ src/main/java/com/example/foodthought/controller/BoardController.java
@@ -4,17 +4,13 @@ import com.example.foodthought.common.dto.ResponseDto;
 import com.example.foodthought.dto.board.CreateBoardRequestDto;
 import com.example.foodthought.dto.board.GetBoardResponseDto;
 import com.example.foodthought.dto.board.UpdateBoardRequestDto;
-import com.example.foodthought.entity.User;
 import com.example.foodthought.security.UserDetailsImpl;
 import com.example.foodthought.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
--- src/main/java/com/example/foodthought/service/BoardService.java
+++ src/main/java/com/example/foodthought/service/BoardService.java
@@ -8,13 +8,11 @@ import com.example.foodthought.entity.User;
 import com.example.foodthought.repository.BoardRepository;
 import com.example.foodthought.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
