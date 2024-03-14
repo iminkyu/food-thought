c53a484df7f9549a5e47ce907afcf080ae9368ca
iminkyu <alsrb18080@gmail.com>
2024-03-14 19:27:06 +0900

    fix : Comment paging 처리 수정

--- src/main/java/com/example/foodthought/repository/CommentRepository.java
+++ src/main/java/com/example/foodthought/repository/CommentRepository.java
@@ -4,10 +4,11 @@ import com.example.foodthought.entity.Comment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
-public interface CommentRepository extends JpaRepository<Comment, Long> {
+public interface CommentRepository extends JpaRepository<Comment, Long>, PagingAndSortingRepository<Comment, Long> {
 //    List<Comment> findByBoardId(Long boardId);
     Page<Comment> findByBoardIdAndParentCommentIsNull(Long boardId, Pageable pageable);
 
