f3aeaac69ca6a47ed69a39e4cc8353dad9ec6c9b
iminkyu <alsrb18080@gmail.com>
2024-03-17 00:01:57 +0900

    fix : Board 쿼리Dsl 수정

--- src/main/java/com/example/foodthought/dto/board/GetBoardResponseDto.java
+++ src/main/java/com/example/foodthought/dto/board/GetBoardResponseDto.java
@@ -5,12 +5,15 @@ import lombok.*;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class GetBoardResponseDto {
     private String title;
     private String author;
     private String publisher;
     private String image;
     private String category;
+    private String userId;
     private String contents;
 }
 
